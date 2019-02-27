8d134e1f5d1913d15340fc40d12baf2ac0f59bca
gk400282 <gk400282@gmail.com>
2019-02-27 16:39:35 +0530

    first commit

--- firstflaskapp.py
+++ firstflaskapp.py
@@ -19,20 +19,5 @@ def dated_url_for(endpoint, **values):
 
 
 @app.route('/')
-def hey_world():
-    return "Hey, World's stupid citizen! I have come to take  shelter and food."
-
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-    	return jsonify({"hello": "world"})
-    else:
-        return "Log In Form"
-
-@app.route('/<name>')
-def hello_world(name=None):
-    return render_template('index.html', name=name)
-
-@app.route('/bye/')
-def bye_world():
-    return "Bye, World's stupid citizens! I have come to take  shelter and food."
+def index():
+    return render_template('index.html')
